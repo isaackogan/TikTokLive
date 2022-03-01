261c799007763a9ab8b2da7476ad7ca0ead38a0b
Isaac Kogan <isaacikogan@gmail.com>
2022-02-28 20:02:01 -0500

    Modify debug.py example

--- examples/debug.py
+++ examples/debug.py
@@ -1,14 +1,22 @@
 from TikTokLive import TikTokLiveClient
-from TikTokLive.types.events import AbstractEvent
+from TikTokLive.types.events import AbstractEvent, GiftEvent
 
 # Instantiate the client with debug param as true
-client: TikTokLiveClient = TikTokLiveClient(unique_id="@patuda_donbazz", debug=True)
+client: TikTokLiveClient = TikTokLiveClient(unique_id="@dailystreamzzz", debug=True)
 
 
+@client.on("gift")
+async def on_gift(event: GiftEvent):
+    # When gifts are sent
+    print(event.user.uniqueId, event.gift.extended_gift.name)
+
+
+# Only fires when debug is set to True
 @client.on("debug")
 async def on_debug(event: AbstractEvent):
     # Receive the raw, unprocessed event dictionaries
-    print("Raw Event Payload:", event.as_dict)
+    if "gift" in event.as_dict.keys():
+        print(event.as_dict)
 
 
 if __name__ == '__main__':
