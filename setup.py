86e2907232393e5836766fce59f913a503d37d69
Isaac Kogan <isaacikogan@gmail.com>
2022-03-07 19:23:47 -0500

    Update versioning

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="TikTokLive",
     packages=setuptools.find_packages(),
-    version="0.7.1",
+    version="0.7.2",
     license="MIT",
     description="TikTok Live Connection Client (Unofficial)",
     author="Isaac Kogan",
@@ -17,7 +17,7 @@ setuptools.setup(
     url="https://github.com/isaackogan/TikTok-Live-Connector",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/isaackogan/TikTokLive/releases/tag/v0.7.1",
+    download_url="https://github.com/isaackogan/TikTokLive/releases/tag/v0.7.2",
     keywords=["tiktok", "tiktok live", "python3", "api", "unofficial"],
     install_requires=[
         "aiohttp>=3.8",  # Make requests
