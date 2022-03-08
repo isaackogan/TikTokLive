a2b2ec5e92dd8a84ed17aaca0c1f9ec7b06903ff
Isaac Kogan <isaacikogan@gmail.com>
2022-03-07 19:21:48 -0500

    Update setup.py versioning

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="TikTokLive",
     packages=setuptools.find_packages(),
-    version="0.7.0",
+    version="0.7.1",
     license="MIT",
     description="TikTok Live Connection Client (Unofficial)",
     author="Isaac Kogan",
