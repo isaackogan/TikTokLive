a268cb348d0b43d5cd0ac97511381573a0ec4f02
Isaac Kogan <isaacikogan@gmail.com>
2022-03-16 18:46:31 -0400

    Add Docs?

---
+++ docs/source/TikTokLive.rst
@@ -0,0 +1,31 @@
+TikTokLive package
+==================
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   TikTokLive.client
+   TikTokLive.proto
+   TikTokLive.types
+
+Submodules
+----------
+
+TikTokLive.utils module
+-----------------------
+
+.. automodule:: TikTokLive.utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: TikTokLive
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ author = 'Isaac Kogan'
 # The full version, including alpha/beta/rc tags
 release = 'Version 0.7.5'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -41,13 +40,12 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
