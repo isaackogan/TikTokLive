d0d063fb0ec5b63e8f19c7a3b5e9d1ba5e4c9c5b
M. Ajun Setiawan <71912420+setiaone-tech@users.noreply.github.com>
2022-09-15 11:52:01 +0700

    Update download_with_quality_video.py

--- examples/download_with_quality_video.py
+++ examples/download_with_quality_video.py
@@ -15,7 +15,11 @@ async def on_connect(_: ConnectEvent):
     client.download(
         path="stream.avi",  # File path to save the download to
         duration=None,  # Download FOREVER. Set to any integer above 1 to download for X seconds
-        quality="hd"  #Quality of the video. This is string you can use sd, ld, hd, or uhd
+        quality="hd"  #Quality of the video. This is string you can use sd, ld, hd, or uhd.
+        #sd = 480p vbrate = 800000
+        #ld = 480p vbrate = 500000
+        #hd = 540p vbrate = 1000000
+        #uhd = 720p vbrate = 1000000
     )
 
     # Stop downloading after 10 seconds.
