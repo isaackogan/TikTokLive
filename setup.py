cbfd9a1e6e590cde2672ac69c49ba081ff2f2336
isaac <isaacikogan@gmail.com>
2022-10-05 20:38:08 -0400

    Versioning for v4.3.8 🎊

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="TikTokLive",
     packages=setuptools.find_packages(),
-    version="4.3.7",
+    version="4.3.8",
     license="MIT",
     description="TikTok Live Connection Client",
     author="Isaac Kogan",
@@ -17,7 +17,7 @@ setuptools.setup(
     url="https://github.com/isaackogan/TikTokLive",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/isaackogan/TikTokLive/releases/tag/v4.3.7",
+    download_url="https://github.com/isaackogan/TikTokLive/releases/tag/v4.3.8",
     keywords=["tiktok", "tiktok live", "python3", "api", "unofficial"],
     install_requires=[
         "httpx>=0.23.0",  # Make requests
