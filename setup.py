495c0123205a4e6c7b3f98bf5478212a1d7b49ce
isaackogan <isaacikogan@gmail.com>
2024-04-10 00:52:01 -0400

    Update versioning

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ manifest: dict = {
     "name": "TikTokLive",
     "license": "MIT",
     "author": "Isaac Kogan",
-    "version": "6.0.4",
+    "version": "6.0.5",
     "email": "info@isaackogan.com"
 }
 
