7750cc70fc4e5a81ddf72b43ba3e5a1bbe830dc2
isaackogan <isaacikogan@gmail.com>
2024-04-10 00:48:51 -0400

    Add debug log for broken proto and add new ExtendedUser attributes: - gifter_level - member_rank - member_level - is_top_gifter - is_moderator - is_subscriber - is_friend - subscriber_badge

--- TikTokLive/proto/custom_proto.py
+++ TikTokLive/proto/custom_proto.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
-from typing import Type, TypeVar
+# noinspection PyUnresolvedReferences
+import re
+# noinspection PyUnresolvedReferences
+from typing import Optional, List, Type, TypeVar, Tuple
 
+# noinspection PyUnresolvedReferences
 import betterproto
 
-from TikTokLive.proto import User, GiftStruct
+from TikTokLive.proto import *
+from TikTokLive.proto.proto_utils import badge_match_user, SUBSCRIBER_BADGE_PATTERN, MODERATOR_BADGE_PATTERN, \
+    TOP_GIFTER_BADGE_PATTERN, MEMBER_LEVEL_BADGE_PATTERN, GIFTER_LEVEL_BADGE_PATTERN
 
 # "MessageType" is a proto enum field.
 # This underscore is the difference between life & death, because if you shadow the proto field,
@@ -62,6 +68,135 @@ class ExtendedUser(User):
 
         return self.display_id
 
+    @property
+    def is_friend(self) -> bool:
+        """
+        Is the user friends with the streamer
+
+        :return: Whether the user is friends with the streamer
+
+        """
+
+        if self.follow_info.follow_status is None:
+            return False
+
+        return (self.follow_info.follow_status or 0) >= 2
+
+    @property
+    def subscriber_badge(self) -> Optional[BadgeStruct]:
+        """
+        Retrieve the subscriber badge of a user
+
+        :return: The user's subscriber badge
+
+        """
+
+        matches: List[Tuple[re.Match, BadgeStruct]] = badge_match_user(
+            user=self,
+            p=SUBSCRIBER_BADGE_PATTERN
+        )
+
+        return matches[0] if matches else None
+
+    @property
+    def is_subscriber(self) -> bool:
+        """
+        Is the user subscribed to the streamer
+
+        :return: Whether the user has the subscriber badge
+
+        """
+
+        return bool(self.subscriber_badge)
+
+    @property
+    def is_moderator(self) -> bool:
+        """
+        Is the user a moderator in the stream
+
+        :return: Whether the user has the moderator badge
+
+        """
+
+        return bool(
+            badge_match_user(
+                user=self,
+                p=MODERATOR_BADGE_PATTERN
+            )
+        )
+
+    @property
+    def is_top_gifter(self) -> bool:
+        """
+        Is the user a top gifter in the stream
+
+        :return: Whether the user has the top gifter badge
+
+        """
+
+        return bool(
+            badge_match_user(
+                user=self,
+                p=TOP_GIFTER_BADGE_PATTERN
+            )
+        )
+
+    @property
+    def member_level(self) -> Optional[int]:
+        """
+        What is the user's "member level" in the stream? This is a number.
+
+        :return: The parsed member level badge
+        """
+
+        matches: list[tuple[re.Match, BadgeStruct]] = badge_match_user(
+            user=self,
+            p=MEMBER_LEVEL_BADGE_PATTERN
+        )
+
+        if len(matches) > 0:
+            return int(matches[0][0].group(1))
+
+        return None
+
+    @property
+    def member_rank(self) -> Optional[str]:
+        """
+        What is the user's "member rank" in the stream? These are roman numerals.
+
+        :return: The parsed member rank from the member level badge
+
+        """
+
+        matches: list[tuple[re.Match, BadgeStruct]] = badge_match_user(
+            user=self,
+            p=MEMBER_LEVEL_BADGE_PATTERN
+        )
+
+        if len(matches) > 0:
+            return matches[0][1].combine.str
+
+        return None
+
+    @property
+    def gifter_level(self) -> Optional[int]:
+        """
+        What is the user's "gifter level" in the stream? An actual number specific to their level.
+
+        :return: The parsed gifter level from the gifter level badge
+
+        """
+
+        matches: list[tuple[re.Match, BadgeStruct]] = badge_match_user(
+            user=self,
+            p=GIFTER_LEVEL_BADGE_PATTERN
+        )
+
+        if len(matches) > 0:
+            return int(matches[0][1].combine.str)
+
+        return None
+
 
 @proto_extension
 class ExtendedGiftStruct(GiftStruct):
@@ -80,4 +215,3 @@ class ExtendedGiftStruct(GiftStruct):
         """
 
         return self.type == 1
-
