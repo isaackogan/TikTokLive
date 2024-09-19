22e7ec269e812dbd0f38082047bf435f66a298da
isaackogan <isaacikogan@gmail.com>
2024-09-18 23:01:44 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ manifest: dict = {
     "name": "TikTokLive",
     "license": "MIT",
     "author": "Isaac Kogan",
-    "version": "6.1.3",
+    "version": "6.1.4",
     "email": "info@isaackogan.com"
 }
 
