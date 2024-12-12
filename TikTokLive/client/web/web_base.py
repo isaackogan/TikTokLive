c85e4c3d5e431cfb1d085f2de707267e081abf9a
Steam Powered Taco <steampoweredtaco@gmail.com>
2024-12-11 20:33:44 -0700

    Remove usage of httpx removed proxies keyword, now uses proxy keyword argument.

--- TikTokLive/client/web/web_base.py
+++ TikTokLive/client/web/web_base.py
@@ -66,7 +66,7 @@ class TikTokHTTPClient:
         }
 
         return AsyncClient(
-            proxies=proxy,
+            proxy=proxy,
             cookies=self.cookies,
             **httpx_kwargs
         )
