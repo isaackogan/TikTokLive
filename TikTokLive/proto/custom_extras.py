047d80cc10ac5419625595eced0e741d4ff17f98
isaackogan <isaacikogan@gmail.com>
2025-03-26 21:38:25 -0400

    Add heartbeat protobuf

--- TikTokLive/proto/custom_extras.py
+++ TikTokLive/proto/custom_extras.py
@@ -38,3 +38,39 @@ class WebcastPushFrame(betterproto.Message):
     payload_encoding: str = betterproto.string_field(6)
     payload_type: str = betterproto.string_field(7)
     payload: bytes = betterproto.bytes_field(8)
+
+
+@dataclass(eq=False, repr=False)
+class HeartbeatFrameRoomInfo(betterproto.Message):
+    room_id: int = betterproto.uint64_field(1)
+
+
+@dataclass(eq=False, repr=False)
+class HeartbeatFrameMetadataField6(betterproto.Message):
+    unknown_1: int = betterproto.uint32_field(14)
+
+
+@dataclass(eq=False, repr=False)
+class HeartbeatFrameMetadataField7(betterproto.Message):
+    unknown_1: int = betterproto.uint32_field(13)
+
+
+@dataclass(eq=False, repr=False)
+class HeartbeatFrame(betterproto.Message):
+    """Heartbeat Keepalive Message"""
+
+    metadata_field_6: HeartbeatFrameMetadataField6 = betterproto.message_field(6)
+    metadata_field_7: HeartbeatFrameMetadataField7 = betterproto.message_field(7)
+    room_info: HeartbeatFrameRoomInfo = betterproto.message_field(8)
+
+    @classmethod
+    def from_defaults(cls, room_id: int) -> "HeartbeatFrame":
+        """
+        Generate a HeartbeatFrame with default values
+        """
+
+        return cls(
+            metadata_field_6=HeartbeatFrameMetadataField6(unknown_1=98),
+            metadata_field_7=HeartbeatFrameMetadataField7(unknown_1=98),
+            room_info=HeartbeatFrameRoomInfo(room_id=room_id)
+        )
