ffbbcafaaa5cf7561d417d196ece8e99f823ca78
isaackogan <isaacikogan@gmail.com>
2025-05-10 19:44:58 -0400

    Support sending messages

--- TikTokLive/client/web/routes/send_room_chat.py
+++ TikTokLive/client/web/routes/send_room_chat.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 import httpx
-from urllib.parse import quote
 
 from TikTokLive.client.web.web_base import ClientRoute
 from TikTokLive.client.web.web_settings import WebDefaults
@@ -12,23 +11,19 @@ class SendRoomChatRoute(ClientRoute):
     async def __call__(
             self,
             content: str,
-            room_id: Optional[int] = None
+            room_id: Optional[int] = None,
+            session_id: Optional[str] = None,
     ) -> dict:
-        """
-        Send a chat message to a TikTok room
 
-        """
+        payload: dict = {
+            "roomId": room_id or self._web.params['room_id'],
+            "content": content,
+            "sessionId": ""
+        }
 
-        extra_params: dict = {"content": quote(content, safe='?%')}
-
-        if room_id is not None:
-            extra_params["room_id"] = room_id
-
-        response: httpx.Response = await self._web.post(
-            sign_url=True,
-            url=WebDefaults.tiktok_webcast_url + "/room/chat/",
-            extra_params=extra_params,
-            http_backend='curl_cffi'
+        response: httpx.Response = await self._web.signer.client.post(
+            url=WebDefaults.tiktok_sign_url + "/webcast/chat/",
+            json=payload,
         )
 
         return response.json()
