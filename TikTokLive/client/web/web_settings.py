f80ab0e4168701f97a1acc4c5124cb5e6c0b7fa5
isaackogan <isaacikogan@gmail.com>
2025-10-25 21:48:15 -0400

    chore: fix support for env-based fallbacks

--- TikTokLive/client/web/web_settings.py
+++ TikTokLive/client/web/web_settings.py
@@ -1,3 +1,4 @@
+import os
 import random
 import urllib.parse
 from dataclasses import dataclass, field
--- TikTokLive/client/web/web_signer.py
+++ TikTokLive/client/web/web_signer.py
@@ -53,8 +53,8 @@ class TikTokSigner:
 
         """
 
-        self._sign_api_key: Optional[str] = sign_api_key or WebDefaults.tiktok_sign_api_key or os.environ.get("SIGN_API_KEY")
-        self._sign_api_base: str = sign_api_base or WebDefaults.tiktok_sign_url or os.environ.get("SIGN_API_URL")
+        self._sign_api_key: Optional[str] = sign_api_key or os.environ.get("SIGN_API_KEY") or WebDefaults.tiktok_sign_api_key
+        self._sign_api_base: str = sign_api_base or os.environ.get("SIGN_API_URL") or WebDefaults.tiktok_sign_url
 
         initial_headers: dict[str, str] = {
             "User-Agent": f"TikTokLive.py/{PACKAGE_VERSION}"
